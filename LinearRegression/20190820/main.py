6e3ed1821131c44862d7a0a20e34255eee393d2b
LinsVert <lindebiaoldb@gmail.com>
2019-08-22 18:07:23 +0800

    add

--- LinearRegression/20190820/main.py
+++ LinearRegression/20190820/main.py
@@ -35,3 +35,24 @@ plt.scatter(x, y, s=5, c='red', label = 'Predicted Regression Line')
 plt.xlabel('Living Space (sqft)')
 plt.ylabel('Price ($)')
 plt.show()
+
+# 定义预测函数 h = z + z1 * x 预测为线性
+# 定义学习率 a
+a = 0.01
+# 定义初始值
+z = 0
+z1 = 0
+# 数量长度
+m = len(x)
+# 定义极限
+minLimit = 1e3
+
+def getH(z, z1, x):
+    return z + z1 * x
+def getZ(self, z, z1, x, y, m, a):
+    return 0
+def getZ1(self, z, z1, x, y, m, a):
+    return 0
+def getAllZ(self, z, z1, x, y, m, a, minLimit):
+    return 0
+
