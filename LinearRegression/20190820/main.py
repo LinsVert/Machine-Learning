db56704520786bc6f7e119f25587eecd7c9eef4d
LinsVert <lindebiaoldb@gmail.com>
2019-08-23 14:55:05 +0800

    add LinearRegression

--- LinearRegression/20190820/main.py
+++ LinearRegression/20190820/main.py
@@ -5,6 +5,7 @@
 # 这个dataset 是房价相关的
 
 import os
+import math
 import pandas as pd
 import matplotlib.pyplot as plt
 from numpy import *  
@@ -28,31 +29,83 @@ for a in alt:
     y.append(a[0])
 x, y = array(x),array(y)
 
-plt.figure()
+# plt.figure()
 
-plt.scatter(x, y, s=5, c='red', label = 'Predicted Regression Line')
+# plt.scatter(x, y, s=5, c='red', label = 'Predicted Regression Line')
 
-plt.xlabel('Living Space (sqft)')
-plt.ylabel('Price ($)')
-plt.show()
+# plt.xlabel('Living Space (sqft)')
+# plt.ylabel('Price ($)')
+# plt.show()
 
 # 定义预测函数 h = z + z1 * x 预测为线性
 # 定义学习率 a
 a = 0.01
 # 定义初始值
 z = 0
-z1 = 0
+z1 = 50
 # 数量长度
+x = x / 1000
+y = y / 1000
+
 m = len(x)
 # 定义极限
-minLimit = 1e3
+min_limit = 1e-3
 
-def getH(z, z1, x):
-    return z + z1 * x
-def getZ(self, z, z1, x, y, m, a):
-    return 0
-def getZ1(self, z, z1, x, y, m, a):
-    return 0
-def getAllZ(self, z, z1, x, y, m, a, minLimit):
-    return 0
+def getH(z, z1, x, y):
+    return z + z1 * x - y
 
+# 获取m
+def getZ(z, z1, x, y, m):
+    sum = 0 
+    for i in range(m):
+        h = getH(z, z1, x[i], y[i])
+        sum = sum + h
+    sum = sum * (1./ m)
+    # newZ = z - sum
+    return sum
+def getZ1(z, z1, x, y, m):
+    sum = 0 
+    for i in range(m):
+        h = getH(z, z1, x[i], y[i])
+        h = h * x[i]
+        sum = sum + h
+    sum = sum * (1./ m)
+    # newZ1 = z1 - sum
+    return sum
+def getAllZ(z, z1, x, y, m, a, min_limit):
+    newZ = getZ(z, z1, x, y, m)
+    newZ1 = getZ1(z, z1, x, y, m)
+    while abs(newZ) >= min_limit and abs(newZ1) >= min_limit:
+        print('z z1', z, z1)
+        print('new z new z1', newZ, newZ1)
+        z = z - a * newZ
+        z1 = z1 - a * newZ1
+        newZ = getZ(z, z1, x, y, m)
+        newZ1 = getZ1(z, z1, x, y, m)
+    print('new z', newZ)
+    print('new z1', newZ1)
+    print('z z1', z, z1)
+    return [z, z1]
+
+result = getAllZ(z, z1, x, y, m, a, min_limit)
+
+#10条数据 z = -13.80519903101423 z1 = 225.65934680567906
+#全部数据 z =  -43.56346904629207 280.61642450710656
+def testY(x, result):
+    return (result[0] + result[1] * x);
+x = x * 1000
+y = y * 1000
+text_y = []
+for i in x:
+    # print(i)
+    text_y.append(testY(i, result))
+
+plt.figure()
+plt.scatter(x, y, s=5, c='red', label='Data')
+plt.plot(x, text_y, c='darkgreen', label='Predicted Regression Line')
+plt.xlabel('Living Space (sqft)')
+plt.ylabel('Price ($)')
+plt.xticks(fontsize=13)
+plt.yticks(fontsize=13)
+plt.legend()
+plt.show()
